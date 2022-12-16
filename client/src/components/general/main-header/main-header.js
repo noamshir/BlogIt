56e0b2b5b113aa83dadadc266bac9cd9fb9cceaf
Noam Dev <noamshirm@gmail.com>
2022-12-16 13:35:15 +0200

    Add: responsive main header nav bar:

--- client/src/components/general/main-header/main-header.js
+++ client/src/components/general/main-header/main-header.js
@@ -4,6 +4,7 @@ import Container from 'react-bootstrap/Container'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Button from 'react-bootstrap/Button'
+import { Offcanvas } from 'react-bootstrap'
 
 import { Logo } from '../logo'
 import { LoginModal } from '../login-modal'
@@ -17,7 +18,7 @@ const MainHeader = () => {
   }
 
   return (
-    <Navbar bg='dark' variant='dark' className='sticky'>
+    <Navbar bg='dark' variant='dark' expand='md' className='sticky'>
       <Container className='main-header-container'>
         <Navbar.Brand href='/' className='logo-and-name-container flex aic'>
           <Logo />
@@ -25,15 +26,32 @@ const MainHeader = () => {
             Blog<span className='it-span'>IT</span>
           </h1>
         </Navbar.Brand>
-        <Nav activeKey={activeKey} className='flex aic'>
-          <Nav.Link href='/'>Home</Nav.Link>
-          <Nav.Link href='/create'>Create your own</Nav.Link>
-          <Nav.Item className='join-nav-item'>
-            <Button variant='primary' size='sm' onClick={toggleModal}>
-              JOIN
-            </Button>
-          </Nav.Item>
-        </Nav>
+        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Offcanvas
+          id={`offcanvasNavbar-expand-md`}
+          aria-labelledby={`offcanvasNavbarLabel-expand-md`}
+          placement='end'
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-md`}>
+              Menu
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav
+              activeKey={activeKey}
+              className='justify-content-end flex-grow-1 pe-3 main-header-nav'
+            >
+              <Nav.Link href='/'>Home</Nav.Link>
+              <Nav.Link href='/create'>Create your own</Nav.Link>
+              <Nav.Item className='join-nav-item'>
+                <Button variant='primary' size='sm' onClick={toggleModal}>
+                  JOIN
+                </Button>
+              </Nav.Item>
+            </Nav>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
       </Container>
       <LoginModal show={showModal} onHide={toggleModal} />
     </Navbar>
